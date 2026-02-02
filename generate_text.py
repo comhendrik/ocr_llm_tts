b9d3526c34ff8324d399a1cdb749d4e71046d44a
Niklas <niklas.herrmann@std.dhsh.de>
2026-02-02 16:37:44 +0100

    updating test slogans

--- generate_text.py
+++ generate_text.py
@@ -1,12 +1,11 @@
 import wave
 from piper import PiperVoice
 
-# Wichtig unter Windows: raw string r"..." oder doppelte Backslashes
 VOICE_PATH = "piperVoices/de_DE-thorsten-medium.onnx"
 
 voice = PiperVoice.load(VOICE_PATH)
 
 with wave.open("output.wav", "wb") as wav_file:
-    voice.synthesize_wav("Moin Niklas, das ist ein Test mit Piper.", wav_file)
+    voice.synthesize_wav("Moin zusammen, das ist der erste Test.", wav_file)
 
 print("Fertig: output.wav")
